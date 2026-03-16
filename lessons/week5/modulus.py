09b5a383c7dd3dddf467ec155791e0f20381a698
Bean <rbean@wsd1.org>
2026-03-16 11:17:46 -0500

    added modulus and special characters file.

---
+++ lessons/week5/modulus.py
@@ -0,0 +1,29 @@
+for i in range(1, 11):
+    #print(i)
+    # if i % 3 == 0:
+    #     print(f"{i} can be divided by 3")
+    if i % 2 == 0:
+        print("EVEN", i)
+    elif i % 2 == 1:
+        print("ODD", i)
+    
+
+# num = 459
+
+# if num % 2 == 0:
+#     print("Even", num)
+# else:
+#     print ("Odd", num)
+
+
+# print("11%1",11%1)
+# print("11%2",11%2)
+# print("11%3", 11%3)
+# print("11%4", 11%4)
+# print("11%5", 11%5)
+# print("11%6", 11%6)
+# print("11%7", 11%7)
+# print("11%8", 11%8)
+# print("11%9", 11%9)
+# print("11%10", 11%10)
+# print("11%11", 11%11)
---
+++ lessons/week5/special_characters.py
@@ -0,0 +1,17 @@
+print("Hello there!\nWelcome to March!")
+
+print("Escape\t\tName\t\tWhat it does")
+print("\u0240")
+
+top    = "\u2554" + "\u2550" * 30 + "\u2557"
+middle = "\u2551" + " Welcome to Python! " + "\u2551"
+bottom = "\u255A" + "\u2550" * 20 + "\u255D"
+print(top)
+print(middle)
+print(bottom)
+
+char = "📁"
+print(ord(char))
+print(hex(ord(char)))
+print("\u2713")
+print("\U0001f4c1")
